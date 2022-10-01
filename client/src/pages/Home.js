f897ba08499edb44de77afabc7ed69a4c36bfad8
JackYouk <jackyoukstetter1@gmail.com>
2022-10-01 11:26:39 -0700

    some starter code for the team card component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
 // imports
 
 import { useState } from "react";
+import TeamCard from "../components/TeamCard";
 
 const Home = () => {
     const [homeState, setHomeState] = useState();
@@ -8,9 +9,29 @@ const Home = () => {
 
     }
 
+    const getTeamData = () => {
+        const data = {
+            name: 'Jacks Squad',
+            squadSize: 5,
+            game: 'Warzone',
+            deviceType: 'Xbox',
+            skill: 'Noobs',
+            squadMembers: [{}, {}, {}]
+        }
+        return data;
+    }
+    const data = {
+        name: 'Jacks Squad',
+        squadSize: 5,
+        game: 'Warzone',
+        deviceType: 'Xbox',
+        skill: 'Noobs',
+        squadMembers: [{}, {}, {}]
+    }
+
     return (
         <div>
-
+            <TeamCard data={data}/>  
         </div>
     );
 }
