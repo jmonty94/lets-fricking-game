a459c5884db3585281e42d454ab24d6714bab37d
DH <daniel@vendettistudios.com>
2022-10-01 12:30:41 -0700

    react-router-dom, app route in index

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,29 @@
+
+
 import { createTheme, ThemeProvider } from "@mui/material";
 import LogComponent from "./components/LogComponent";
 import ResponsiveAppBar from "./components/NavBar";
 
 
 function App() {
-  const theme = createTheme({
-    palette: {
-      primary: {
-        main: '#002984',
-      },
-      secondary: {
-        main: '#ff7961'
-      }
-    }
-  })
-  return (
-    <div>
-      <ThemeProvider theme={theme}>
-        <ResponsiveAppBar />
-        <LogComponent />
-      </ThemeProvider>
-    </div>
-  );
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: '#002984',
+            },
+            secondary: {
+                main: '#ff7961'
+            }
+        }
+    })
+    return (
+        <div>
+            <ThemeProvider theme={theme}>
+                        <ResponsiveAppBar />
+                        <LogComponent />
+            </ThemeProvider>
+        </div>
+    );
 }
 
 export default App;
