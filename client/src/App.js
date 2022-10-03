11d79228357efc95be0e41fa6ae0d9213a9fcfde
Snufalufakis <snufalufakis@gmail.com>
2022-10-03 00:33:47 -0700

    Added logo to LoginSignup page
    added logo. working on importing font, added confirm password text area.

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,38 @@
-import {Route, Routes} from 'react-router-dom'
+import { Route, Routes } from "react-router-dom";
 
-import Profile from './pages/Profile'
-import Layout from "./components/Layout"
+import Profile from "./pages/Profile";
+import Layout from "./components/Layout";
 
 import { createTheme, ThemeProvider } from "@mui/material";
 import LogComponent from "./components/LogComponent";
 import ResponsiveAppBar from "./components/NavBar";
 import Home from "./pages/Home";
 
-
 function App() {
-    const theme = createTheme({
-        palette: {
-            primary: {
-                main: '#002984',
-            },
-            secondary: {
-                main: '#ff7961'
-            }
-        }
-    })
-    return (
-        <div>
-            <ThemeProvider theme={theme}>
-                        <ResponsiveAppBar />
-                        <Routes>
-                            <Route 
-                              path="/"
-                              element={<Home />} 
-                            />
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#002984",
+      },
+      secondary: {
+        main: "#ff7961",
+      },
+    },
+  });
+  return (
+    <div>
+      <ThemeProvider theme={theme}>
+        <ResponsiveAppBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
 
-                            <Route 
-                              path="/signinup" 
-                              element={<LogComponent /> } 
-                            />
+          <Route path="/signinup" element={<LogComponent />} />
 
-                            <Route 
-                              path="/profile"
-                              element={<Profile />} 
-                            />
-                        </Routes>
-            </ThemeProvider>
-        </div>
-    );
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </ThemeProvider>
+    </div>
+  );
 }
 
 export default App;
--- client/src/pages/LoginSignup.js
+++ client/src/pages/LoginSignup.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import Logo from "./../assets/images/logo-lfg-black_adobe_express.svg";
 
 // mui
 import * as React from "react";
 import TextField from "@mui/material/TextField";
 import Grid from "@mui/material/Grid";
 import { Button } from "@mui/material";
+import { red } from "@mui/material/colors";
 
 const Login = () => {
   const [formState, setFormState] = useState({
@@ -35,6 +37,14 @@ const Login = () => {
       spacing={3}
     >
       <Grid item xs={12}>
+        <img
+          src={Logo}
+          alt="logo"
+          style={{
+            width: "9vw",
+            position: "relative",
+          }}
+        />
         <h1>Login</h1>
       </Grid>
 
@@ -59,7 +69,17 @@ const Login = () => {
         />
       </Grid>
       <Grid item xs={12}>
-        <Button variant="contained" onClick={handleSubmit()}>
+        <Button
+          variant="contained"
+          onClick={handleSubmit()}
+          style={{
+            fontWeight: "400",
+            fontSize: "11pt",
+            borderRadius: "3px",
+            cursor: "pointer",
+            background: red[700],
+          }}
+        >
           Login
         </Button>
       </Grid>
@@ -97,6 +117,14 @@ const Signup = () => {
       spacing={3}
     >
       <Grid item xs={12}>
+        <img
+          src={Logo}
+          alt="logo"
+          style={{
+            width: "9vw",
+            position: "relative",
+          }}
+        />
         <h1>Sign Up</h1>
       </Grid>
 
@@ -112,8 +140,21 @@ const Signup = () => {
       </Grid>
       <Grid item xs={12}>
         <TextField
-          id="standard-basic"
+          id="standard-password"
           label="Password"
+          variant="standard"
+          type="password"
+          onChange={() => {
+            handleChange();
+          }}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          id="standard-password"
+          label="Password"
+          type="password"
+          placeholder="confirm password"
           variant="standard"
           onChange={() => {
             handleChange();
@@ -121,7 +162,17 @@ const Signup = () => {
         />
       </Grid>
       <Grid item xs={12}>
-        <Button variant="contained" onClick={handleSubmit()}>
+        <Button
+          variant="contained"
+          onClick={handleSubmit()}
+          style={{
+            fontWeight: "400",
+            fontSize: "11pt",
+            borderRadius: "3px",
+            cursor: "pointer",
+            background: red[700],
+          }}
+        >
           Sign up
         </Button>
       </Grid>
