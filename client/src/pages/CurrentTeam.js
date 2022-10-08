7406a3c899b68224a7ae28ffe1a7de4453c6587c
James Montgomery <103618671+jmonty94@users.noreply.github.com>
2022-10-08 02:57:42 -0700

    began work on card

--- client/src/pages/CurrentTeam.js
+++ client/src/pages/CurrentTeam.js
@@ -1,25 +1,66 @@
-import {useQuery} from '@apollo/client';
+import { useQuery } from '@apollo/client';
+import { Box, Card, CardActionArea, CardContent, CardMedia, CircularProgress, Typography } from '@mui/material';
 import { QUERY_MY_TEAM } from '../utils/queries';
 import { QUERY_ME } from '../utils/queries';
 
+import destiny from '../gameImages/destiny.jpeg';
+import madden from '../gameImages/madden.jpeg';
+import nba2k from '../gameImages/nba2k.jpeg';
+import warzone from '../gameImages/warzone.jpeg';
+import wow from '../gameImages/wow.jpeg';
+
 
 const CurrentTeam = () => {
-    const {loading, data} = useQuery(QUERY_MY_TEAM);
-    
+    const { loading, data } = useQuery(QUERY_MY_TEAM);
+    console.log(data);
+    function createImg (data)  {
+        let gameImg;
+        switch (data) {
+            case 'Destiny':
+                return gameImg = destiny;
+            case 'Madden':
+                return gameImg = madden;
+            case 'NBA2K':
+                return gameImg = nba2k;
+            case 'Warzone':
+                return gameImg = warzone;
+            case 'WOW':
+                return gameImg = wow;
+
+            default:
+                break;
+        }
+    }
     return (
         <div>
             {loading ? (
-                <h1>Loading</h1>
+                <Box sx={{ display: 'flex' }}>
+                    <CircularProgress />
+                </Box>
             ) : (
-                <div>
-                    <h1>{data.myTeam.name}</h1>
-                    <h1>{data.myTeam.game}</h1>
-                    <h1>{data.myTeam.deviceType}</h1>
-                    
-                    {/* may be null for owner */}
-                    <h1>{data.myTeam.owner.username}</h1> 
-                    <h1></h1>
-                </div>
+                <Box sx={{ m: 5 }}>
+                    <Card sx={{ display: 'block', justifyContent: 'center', textAlign: 'center', width: "75%", m: 'auto' }}>
+                        <CardActionArea>
+                            <CardMedia
+                                component='img'
+                                height='35%'
+                                image={createImg(data.myTeam.game)}
+
+                            />
+                            <CardContent>
+                            <Typography variant='h2' component='h1'>{data.myTeam.name}</Typography>
+                            <Typography variant='h3' component='h2'>{data.myTeam.game}</Typography>
+                            <Typography>{data.myTeam.deviceType}</Typography>
+
+                            {/* may be null for owner */}
+                            <Typography>{data.myTeam.owner.username}</Typography>
+                            {data.myTeam.squadMembers.map(squadMember => (
+                                console.log(squadMember)
+                            ))}
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                </Box>
             )}
         </div>
     );
